ab7cfd36ca6fe82fcac22edad05e302d3921f4ed
Alex Withington-Smith <alex@awithsmith.com>
2020-12-02 16:13:52 +0000

    added power saver

--- thermostat.js
+++ thermostat.js
@@ -1,14 +1,35 @@
 class Thermostat {
     constructor() {
         this.temperature = 20;
+        this.isPowerSave = true;
     }
 
     up = function() {
-        this.temperature += 1;
+        if (this.isPowerSave && this.temperature < 25) {
+            this.temperature++;
+            return;
+        }
+
+        if (!this.isPowerSave && this.temperature < 32) {
+            this.temperature++;
+            return;
+        }
     }
 
     down = function() {
-        this.temperature -= 1;
+        if (this.temperature > 10) { this.temperature--; };
+    }
+
+    powerSaveOff = function() {
+        this.isPowerSave = false;
+    }
+
+    powerSaveOn = function() {
+        this.isPowerSave = true;
+    }
+
+    reset = function() {
+        this.temperature = 20;
     }
 }
 
--- thermostat.test.js
+++ thermostat.test.js
@@ -12,6 +12,27 @@ describe('Thermostat', () => {
             testThermostat.up();
             expect(testThermostat.temperature).toEqual(21);
         })
+
+        it("won't go above 25 degrees if power saving on", () => {
+            let testThermostat = new Thermostat;
+            for(let i = 0; i < 5; i++) {
+                testThermostat.up();
+            }
+            expect(testThermostat.temperature).toEqual(25);
+            testThermostat.up();
+            expect(testThermostat.temperature).toEqual(25);
+        })
+
+        it("won't go above 32 degrees if power saving is off", () => {
+            let testThermostat = new Thermostat;
+            testThermostat.powerSaveOff();
+            for(let i = 0; i < 12; i++) {
+                testThermostat.up();
+            }
+            expect(testThermostat.temperature).toEqual(32);
+            testThermostat.up();
+            expect(testThermostat.temperature).toEqual(32);
+        })
     })
 
     describe('down()', () => {
@@ -19,6 +40,49 @@ describe('Thermostat', () => {
             let testThermostat = new Thermostat;
             testThermostat.down();
             expect(testThermostat.temperature).toEqual(19);
+        })
+
+        it("won't let temperature reduce lower than 10", () => {
+            let testThermostat = new Thermostat;
+            for(let i = 0; i < 10; i++) {
+                testThermostat.down();
+            }
+            expect(testThermostat.temperature).toEqual(10)
+            testThermostat.down();
+            expect(testThermostat.temperature).toEqual(10)
+        })
+    })
+
+    describe('isPowerSave', () => {
+        it('initializes as true', () => {
+            let testThermostat = new Thermostat;
+            expect(testThermostat.isPowerSave).toEqual(true)
+        })
+
+        it('can be switched off', () => {
+            let testThermostat = new Thermostat;
+            testThermostat.powerSaveOff();
+            expect(testThermostat.isPowerSave).toEqual(false);
+        })
+
+        it('can be switched back on', () => {
+            let testThermostat = new Thermostat;
+            testThermostat.powerSaveOff();
+            expect(testThermostat.isPowerSave).toEqual(false);
+            testThermostat.powerSaveOn();
+            expect(testThermostat.isPowerSave).toEqual(true)
+        })
+    })
+
+    describe('reset()', () => {
+        it('brings temperature back to 20', () => {
+            let testThermostat = new Thermostat;
+            for(let i = 0; i < 10; i++) {
+                testThermostat.down();
+            }
+            expect(testThermostat.temperature).toEqual(10);
+            testThermostat.reset();
+            expect(testThermostat.temperature).toEqual(20);
         })
     })
 })
