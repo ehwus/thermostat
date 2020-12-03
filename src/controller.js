b372eb31604f6dda845dabdb4054a7876a3e8aa7
Alex Withington-Smith <alex@awithsmith.com>
2020-12-03 15:38:51 +0000

    added current temperature

--- src/controller.js
+++ src/controller.js
@@ -1,12 +1,19 @@
 let thermostat = new Thermostat();
 
 $(document).ready(function () {
+  let getTemperature = () => {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', (data) => {
+      $("#current-temp").text(data.main.temp + " degrees");
+    })
+  }
+
   let updateDisplay = () => {
     $(".display").text(String(thermostat.temperature));
     $("#energy-usage").text(thermostat.currentUsage());
   };
 
   updateDisplay();
+  getTemperature();
 
   $("#up").click(() => {
     thermostat.up();
